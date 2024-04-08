15f2679e31331569314ef1e1aec4cbb5b5598b37
Nicolas Beovide <nico1803o@hotmail.com.ar>
2024-04-07 22:13:21 -0300

    Feat component ( LOGIN + CONTACTS )

---
+++ components/Login/Login.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import '../../css/login.css'
+
+const login = () => {
+  return (<>
+    <main>
+    <img src="img/Sin título-1-Recuperado 2.png" alt=""/>
+    <h2>Empezá tu pasión por las plantas</h2>
+
+    <form>
+        <div id="input">
+            <input type="email" name="correo" id="correo" placeholder="Correo electrónico..."/>
+            <div>
+                <input type="password" name="password" id="password" placeholder="Contraseña..."/>
+                <lord-icon
+                    src="https://cdn.lordicon.com/vfczflna.json"
+                    trigger="click"
+                    colors="primary:#121331,secondary:#000000"
+                    class="eye">
+                </lord-icon>
+            </div>
+        </div>
+        <div id="forgotPassword">
+            <a href="#">Olvidé mi contraseña</a>
+        </div>
+        <input type="submit" value="Ingresar" id="submit"/>
+        <div id="createAccount">
+            <p>¿No tenés cuenta?</p>
+            <a href="#">Crear cuenta</a>
+        </div>
+    </form>
+</main>
+
+<aside>
+    <span></span>
+    <h1>Rincón Verde</h1>
+</aside>
+  </>)
+}
+
+export default login
